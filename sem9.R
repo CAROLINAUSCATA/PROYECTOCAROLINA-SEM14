f8bdcdf3287e7d9e36f8cbee6a2f001e7550027d
CAROLINAUSCATA <CAROLINA.USCATA@UPSJB.EDU.PE>
2025-12-07 13:33:57 -0500

    Add files via upload

---
+++ sem9.R
@@ -0,0 +1,16 @@
+#INSTALANDO PAQUETES
+
+install.packages("readr")
+install.packages("dplyr")
+install.packages("ggplot2")
+
+
+library(readr)
+library(dplyr)
+library(ggplot2)
+
+s09_circunf_glucosa <- read_csv("C:/Users/ACER/Downloads/s09_circunf_glucosa.csv")
+View(s09_circunf_glucosa.cvs)
+
+data<- data.frame(s09_circunf_glucosa)
+data
